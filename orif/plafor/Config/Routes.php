8127a4da5c0a195a166db66824bda0eac05b18ae
HeMa <MaximeEugene.Herbez@formation.orif.ch>
2021-05-06 11:21:41 +0200

    added Route for plafor's requests handling

---
+++ orif/plafor/Config/Routes.php
@@ -0,0 +1,11 @@
+<?php
+/**
+ * Routes for Plafor Module
+ *
+ * @author      Orif (ViDi,HeMa)
+ * @link        https://github.com/OrifInformatique
+ * @copyright   Copyright (c), Orif (https://www.orif.ch)
+ */
+
+$routes->add('plafor/admin/(:any)','\Plafor\Controllers\Admin::$1');
+?>
