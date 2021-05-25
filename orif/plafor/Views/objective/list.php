931d81d3c41a1f120af3daf0d716eb8493fde7f8
HeMa <MaximeEugene.Herbez@formation.orif.ch>
2021-05-25 14:39:53 +0200

    added views list, save and view to objective

---
+++ orif/plafor/Views/objective/list.php
@@ -0,0 +1,70 @@
+<?php
+/**
+ * Users List View
+ *
+ * @author      Orif, section informatique (UlSi, ViDi,HeMa)
+ * @link        https://github.com/OrifInformatique/gestion_questionnaires
+ * @copyright   Copyright (c) Orif (http://www.orif.ch)
+ */
+helper('form');
+?>
+<div class="container">
+    <div class="row">
+        <div class="col">
+            <h1 class="title-section"><?= lang('user_lang.title_objective_list'); ?></h1>
+        </div>
+    </div>
+    <div class="row">
+    	<?php if($_SESSION['user_access'] == config('User\Config\UserConfig')->access_lvl_admin): ?>
+        <div class="col-sm-3 text-left">
+            <a href="<?= base_url('plafor/admin/save_objective'); ?>" class="btn btn-primary">
+                <?= lang('common_lang.btn_add'); ?>
+            </a>
+        </div>
+    	<?php endif; ?>
+		<div class="col-sm-3 offset-6">
+			<?=form_checkbox('toggle_deleted', '', $with_archived, [
+				'id' => 'toggle_deleted', 'class' => 'form-check-input'
+			]);?>
+			<?=form_label(lang('common_lang.btn_show_disabled'), 'toggle_deleted', ['class' => 'form-check-label']);?>
+		</div>
+    </div>
+    <div class="row mt-2">
+        <table class="table table-hover">
+        <thead>
+            <tr>
+                <th><?= lang('user_lang.field_objective_name'); ?></th>
+                <th></th>
+                <?php if($_SESSION['user_access'] == config('User\Config\UserConfig')->access_lvl_admin): ?>
+                <th></th>
+                <th></th>
+                <?php endif; ?>
+            </tr>
+        </thead>
+        <tbody id="objectiveslist">
+            <?php foreach($objectives as $objective) { ?>
+                <tr>
+                    <td><a href="<?= base_url('plafor/apprentice/view_objective/'.$objective['id']); ?>"><span class="font-weight-bold"><?= $objective['symbol']?></span> <?= $objective['name']; ?></td>
+                    <td><a href="<?= base_url('plafor/apprentice/view_objective/').$objective['id']?>"><?= lang('common_lang.btn_details')?></a></td>
+                    <?php if($_SESSION['user_access'] == config('User\Config\UserConfig')->access_lvl_admin): ?>
+                    <td><a href="<?= base_url('plafor/admin/save_objective/'.$objective['id']); ?>"><?= lang('common_lang.btn_edit')?></a></td>
+                    <td><a href="<?= base_url('plafor/admin/delete_objective/'.$objective['id']); ?>" class="close">×</td>
+                    <?php endif; ?>
+                </tr>
+            <?php } ?>
+        </tbody>
+        </table>
+    </div>
+</div>
+
+<script>
+$(document).ready(function(){
+    $('#toggle_deleted').change(e => {
+        let checked = e.currentTarget.checked;
+		$.post('<?=base_url();?>admin/list_objective/<?=$id??null?>/'+(+checked), {}, data => {
+            $('#objectiveslist').empty();
+            $('#objectiveslist')[0].innerHTML = $(data).find('#objectiveslist')[0].innerHTML;
+        });
+    });
+});
+</script>
