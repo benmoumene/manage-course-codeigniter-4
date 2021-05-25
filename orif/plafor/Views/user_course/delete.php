fa34b65b385fbc3952966430f3e57daf118be1d3
HeMa <MaximeEugene.Herbez@formation.orif.ch>
2021-05-25 14:36:56 +0200

    updated view delete user_course --- not ended

---
+++ orif/plafor/Views/user_course/delete.php
@@ -0,0 +1,25 @@
+<?php if (!defined('BASEPATH')) exit('No direct script access allowed');
+?>
+<div id="page-content-wrapper">
+    <div class="container">
+        <div class="row">
+            <div class="col-12">
+                <div>
+                    <h1><?= lang('apprentice').' "'.$apprentice->username.'"' ?></h1>
+                    <h1><?= lang('course_plan').' "'.$course_plan->official_name.'"' ?></h1>
+                    <h1><?= lang('status').' "'.$status->name.'"' ?></h1>
+                    <h4><?= lang('what_to_do')?></h4>
+                    <div class = "alert alert-info" ><?= lang('user_course_disable_explanation')?></div>
+                </div>
+                <div class="text-right">
+                    <a href="<?= base_url('apprentice/view_user_course/'.$apprentice->id); ?>" class="btn btn-default">
+                        <?= lang('btn_cancel'); ?>
+                    </a>
+                    <a href="<?= base_url(uri_string().'/1'); ?>" class="btn btn-danger">
+                        <?= lang('btn_disable'); ?>
+                    </a>
+                </div>
+            </div>
+        </div>
+    </div>
+</div>
