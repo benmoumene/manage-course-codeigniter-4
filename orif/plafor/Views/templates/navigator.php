cd9a53d372584ef1852ae9d96622e7430f171326
HeMa <MaximeEugene.Herbez@formation.orif.ch>
2022-01-19 13:16:57 +0100

    fix navigator to redirect on good root

--- orif/plafor/Views/templates/navigator.php
+++ orif/plafor/Views/templates/navigator.php
@@ -6,7 +6,6 @@ else{
 $navigatorLink=service('session')->get('navigator');
 if (isset($navigatorLink)){
     $index=null;
-
     for ($i=count($navigatorLink)-1;$i>=0;$i--){
         if ($navigatorLink[$i]['link']==((current_url().(strlen($_SERVER['QUERY_STRING'])?'?'.$_SERVER['QUERY_STRING']:'')))){
             $index=$i;
@@ -28,13 +27,15 @@ if (isset($navigatorLink)){
 }
 else{
 
-    service('session')->set('navigator',[['link'=>current_url().(strlen($_SERVER['QUERY_STRING'])?'?'.$_SERVER['QUERY_STRING']:''),'title'=>$title]]);
+    service('session')->set('navigator',[['link'=>service('session')->get('_ci_previous_url'),'title'=>''],['link'=>current_url().(strlen($_SERVER['QUERY_STRING'])?'?'.$_SERVER['QUERY_STRING']:''),'title'=>$title]]);
+
+
 }
 ?>
 <span id="navigator">
     <?php 
     $navigatorLink=service('session')->get('navigator');
-    if(count($navigatorLink)>1):?>
+    if(count($navigatorLink)>2):?>
         <a id="navigator-back" class="btn btn-outline-primary mr-2 bi bi-arrow-left-circle" href="<?=$navigatorLink[count($navigatorLink)-2]['link']?>">
             <?= lang("common_lang.btn_back") ?>
         </a>
@@ -42,8 +43,8 @@ else{
             <a href="<?=$navigatorLink[$i]['link']?>"><?=$navigatorLink[$i]['title']?></a>
             <?php echo count($navigatorLink)>1 && $i!=count($navigatorLink)-1 ? '<span class="text-primary"><i class="bi bi-arrow-right-short" ></i></span>' : ''?>
         <?php } ?>
-    <?php else : ?>
-        <a id="navigator-back" class="btn btn-outline-primary mr-2 bi bi-arrow-left-circle" href="<?= base_url("plafor/courseplan/list_course_plan") ?>">
+    <?php else: ?>
+        <a id="navigator-back" class="btn btn-outline-primary mr-2 bi bi-arrow-left-circle" href="<?= $navigatorLink[0]['link'] ?>">
             <?= lang("common_lang.btn_back") ?>
         </a>
     <?php endif;?>
