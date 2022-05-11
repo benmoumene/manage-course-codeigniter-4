7c469a69070d97640b256370f2feed46a4218567
MeSa <42637780+MestourBS@users.noreply.github.com>
2022-05-11 13:49:41 +0200

    Allow non admins to see linked modules

--- orif/plafor/Views/course_plan/view.php
+++ orif/plafor/Views/course_plan/view.php
@@ -51,38 +51,62 @@ $is_admin = service('session')->get('user_access') >= config('\User\Config\UserC
             </table>
         </div>
     </div>
-    <?php if($is_admin): ?>
-        <div class="row">
-            <div class="col-12">
-                <p class="bg-primary text-white"><?=lang('plafor_lang.title_view_modules_linked')?></p>
-            </div>
-            <div class="col-12">
-                <a href="<?= base_url('plafor/module/save_module/0/' . $course_plan['id']); ?>" class="btn btn-primary"><?=lang('common_lang.btn_new_m')?></a>
-                <a href="<?= base_url('plafor/courseplan/link_module/' . $course_plan['id']); ?>" class="btn btn-primary"><?= lang('plafor_lang.change_course_plan_module_links'); ?></a>
-
-                <table class="table table-hover mt-2">
-                    <thead>
-                        <tr>
-                            <th><?= lang('plafor_lang.field_module_official_name'); ?></th>
-                            <th></th>
-                            <th></th>
-                            <th></th>
-                            <th></th>
-                        </tr>
-                    </thead>
-                    <tbody>
-                        <?php foreach ($modules as $module) { ?>
-                            <tr>
-                                <td><a href="<?= base_url('plafor/module/view_module/'.$module['id']); ?>"><span class="font-weight-bold"><?= $module['module_number']; ?></span> <?= $module['official_name']; ?></a></td>
-                                <td><?= lang('plafor_lang.module_is_' . ($module['is_school'] ? '' : 'not_') . 'school'); ?></td>
-                                <td><a href="<?= base_url('plafor/module/view_module/'.$module['id']); ?>"><?= lang('common_lang.btn_details'); ?></a></td>
-                                <td><a href="<?= base_url('plafor/module/save_module/'.$module['id']); ?>"><?= lang('common_lang.btn_edit'); ?></a></td>
-                                <td><a href="<?= base_url('plafor/module/delete_module/'.$module['id']); ?>" class="bi <?= $module['archive'] == null ? 'bi-trash' : 'bi-reply-all-fill' ?>"></a></td>
-                            </tr>
-                        <?php } ?>
-                    </tbody>
-                </table>
-            </div>
+    <div class="row">
+        <div class="col-12">
+            <p class="bg-primary text-white"><?=lang('plafor_lang.title_view_modules_linked')?></p>
         </div>
-    <?php endif; ?>
+        <div class="col-12">
+            <?php if($is_admin): ?>
+            <a href="<?= base_url('plafor/module/save_module/0/' . $course_plan['id']); ?>" class="btn btn-primary"><?=lang('common_lang.btn_new_m')?></a>
+            <a href="<?= base_url('plafor/courseplan/link_module/' . $course_plan['id']); ?>" class="btn btn-primary"><?= lang('plafor_lang.change_course_plan_module_links'); ?></a>
+            <?php endif; ?>
+
+            <table class="table table-hover mt-2">
+                <thead>
+                    <tr>
+                        <th><?= lang('plafor_lang.field_module_official_name'); ?></th>
+                        <th></th>
+                        <th></th>
+                        <th></th>
+                        <th></th>
+                    </tr>
+                </thead>
+                <tbody>
+                    <?php foreach ($modules as $module) { ?>
+                        <tr>
+                            <td>
+                                <?php if($is_admin): ?>
+                                <a href="<?= base_url('plafor/module/view_module/'.$module['id']); ?>">
+                                <?php endif; ?>
+                                    <span class="font-weight-bold"><?= $module['module_number']; ?></span> <?= $module['official_name']; ?>
+                                <?php if($is_admin): ?>
+                                </a>
+                                <?php endif; ?>
+                            </td>
+                            <td><?= lang('plafor_lang.module_is_' . ($module['is_school'] ? '' : 'not_') . 'school'); ?></td>
+                            <td>
+                                <?php if($is_admin): ?>
+                                <a href="<?= base_url('plafor/module/view_module/'.$module['id']); ?>">
+                                    <?= lang('common_lang.btn_details'); ?>
+                                </a>
+                                <?php endif; ?>
+                            </td>
+                            <td>
+                                <?php if($is_admin): ?>
+                                <a href="<?= base_url('plafor/module/save_module/'.$module['id']); ?>">
+                                    <?= lang('common_lang.btn_edit'); ?>
+                                </a>
+                                <?php endif; ?>
+                            </td>
+                            <td>
+                                <?php if($is_admin): ?>
+                                <a href="<?= base_url('plafor/module/delete_module/'.$module['id']); ?>" class="bi <?= $module['archive'] == null ? 'bi-trash' : 'bi-reply-all-fill' ?>"></a>
+                                <?php endif; ?>
+                            </td>
+                        </tr>
+                    <?php } ?>
+                </tbody>
+            </table>
+        </div>
+    </div>
 </div>
